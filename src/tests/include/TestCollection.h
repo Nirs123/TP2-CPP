929298febdb4e16d8be3dcb938fca55172bb9dc3
Siwa12100 <jean.marcillac12@gmail.com>
2025-10-06 09:33:55 +0000

    travail classe collection

--- src/tests/include/TestCollection.h
+++ src/tests/include/TestCollection.h
@@ -3,5 +3,10 @@
 
 void lancementTestsCollection();
 void testCollectionInstanciation();
+void testCollectionAjoutDeTrajetsSimples();
+void testCollectionSuppressionTrajetsSimples();
+void testCollectionSuppressionAIndexInvalides();
+void TestRechercheValideDansCollectionDeTrajetsSimples();
+void testRecherchesInvalidesDansCollectionDeTrajetsSimples();
 
 #endif // TEST_COLLECTION_H
