2a85af7849726d92273fb1f5619c9febcf0cbdbf
Siwa12100 <jean.marcillac12@gmail.com>
2025-10-06 08:40:25 +0000

    tests collection

--- src/tests/sources/TestCollection.cpp
+++ src/tests/sources/TestCollection.cpp
@@ -1,5 +1,7 @@
 #include "../include/TestCollection.h"
 #include "../../modeles/include/Collection.h"
+#include "../../modeles/include/TrajetSimple.h"
+#include "../../modeles/include/MoyenTransport.h"
 #include <iostream>
 using namespace std;
 
@@ -19,6 +21,16 @@ void testCollectionInstanciation() {
     cout << "\tSuccès de la destruction de la Collection." << endl;
 }
 
+void testCollectionAjoutTrajetSimple() {
+    cout << "[TEST] Ajout d'un trajet simple à une Collection." << endl;
+
+    Collection* c = new Collection(5);
+    TrajetSimple* ts = new TrajetSimple("Paris", "Lyon", Bateau);
+    c->Ajouter(ts);
+    delete c; // La collection doit gérer la destruction des trajets qu'elle contient
+}
+
 void lancementTestsCollection() {
     testCollectionInstanciation();
+    testCollectionAjoutTrajetSimple();
 }
